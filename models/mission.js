03c64344d8786b72136c065c0153fe3e7c44e1ee
John M. Larkin <jlarkin@whitworth.edu>
2018-07-11 22:03:45 -0700

    Create mission and pod schema

---
+++ models/mission.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose'),
+      Schema = mongoose.Schema;
+
+const podDataTypes = [
+  ['uint8',1],
+  ['int8',1],
+  ['uint16',2],
+  ['int16',2],
+  ['uint32',4],
+  ['int32',4],
+  ['float',4],
+  ['double',8]
+];
+
+const PodSchema = new Schema({
+  podDescription: String,
+  dataDescriptions: [String],
+  dataTypes: [Number]
+})
+
+const Mission = new Schema({
+  missionID: Number,
+  launchCode: String,
+  description: String,
+  organizationID: Schema.Types.ObjectId,
+  podManifest: [PodSchema],
+  status: {
+    type: String,
+    enum: ['planned','active','archived'],
+    default: 'planned'
+  },
+  launchDate: Date
+})
+
+module.exports = {
+  mongoose.model('PodSchema',PodSchema),
+  mongoose.model('Mission',Mission),
+  podDataTypes
+}
